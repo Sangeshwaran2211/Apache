d011bac48b6193e1c437edc38949a0670fc5fdc2
Sangeshwaran2211 <84615221+Sangeshwaran2211@users.noreply.github.com>
2022-06-08 15:47:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+ provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install httpd && sudo systemctl start httpd",
+      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
+      "sudo mv index.html /var/www/html/"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
