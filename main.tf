712e9cc452f1be3e4884ff5670a7f5c7bfd6fbe6
Sangeshwaran2211 <84615221+Sangeshwaran2211@users.noreply.github.com>
2022-06-08 20:12:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,6 @@
  provisioner "remote-exec" {
     inline = [
       "sudo yum -y install httpd && sudo systemctl start httpd",
-      "echo '<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {background="purple";}
-</style>
-<title>Hi<title>
-</head>
-<body>
-<h1>Yay</h1>
-<p>yay is a word used to describe that your happy!</p>
-</body>
-</html>' index.html",
+      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
       "sudo mv index.html /var/www/html/"
     ]
- }
-    
