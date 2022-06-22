806d8b80430b7fb1c861bfe849370d49bd7e4133
Sangeshwaran2211 <84615221+Sangeshwaran2211@users.noreply.github.com>
2022-06-22 13:48:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,27 @@
+variable "myvpc" {
+     default= "10.0.0.0/16"
+  
+}
+variable "pubsub" {
+    default = "10.0.1.0/24"
+  
+}
 
+variable "privsub" {
+    default = "10.0.2.0/24"
+  
+}
+variable "pubrt" {
+    default = "0.0.0.0/0"
+  
+}
+variable "privrt" {
+    default = "0.0.0.0/0"
+  
+}
+variable "ami" {
+  default = "ami-0bd6906508e74f692"
+}
+variable "instance_type" {
+  default =  "t2.micro" 
+}
